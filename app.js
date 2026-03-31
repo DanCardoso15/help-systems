8010cbc707ac04fa1db6f0d47c0b30b569a5100c
DanCardoso15 <dancardoso15@hotmail.com>
2026-03-31 14:32:55 -0300

    RF04 - Responder solicitacoes

--- app.js
+++ app.js
@@ -28,9 +28,11 @@ app.use((req, res, next) => {
 const authRoutes = require('./routes/auth');
 const homeRoutes = require('./routes/home');
 const solicitacoesRoutes = require('./routes/solicitacoes');
+const respostasRoutes = require('./routes/respostas');
 app.use('/', authRoutes);
 app.use('/', homeRoutes);
 app.use('/', solicitacoesRoutes);
+app.use('/', respostasRoutes);
 
 app.get('/', (req, res) => {
     if (!req.session.usuario) {
